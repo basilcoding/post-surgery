b2d698ee83da532bc0624070f04a9668f1cd9e4a
basilcoding <basilshahul234@gmail.com>
2025-10-05 15:05:05 +0530

    Now the user id is obtained from req.user intead of req.body to adhere to best practices

--- server/src/controllers/chatbot.controller.js
+++ server/src/controllers/chatbot.controller.js
@@ -7,7 +7,9 @@ import { chatbot } from '../utils/chatbotCore.util.js'
 
 export const sendMessage = async (req, res) => {
 
-    const { userId, message } = req.body;
+    // const { userId, message } = req.body;
+    const userId = req.user._id; // from the verified token
+    const { message } = req.body; // from the client request body
 
     // console.log(chats[userId]) // undefined
     const isEnd = /^(?:quit|quite|quitt|quti|qit|qut|quyt|kwit|qiut|qiot|qujt|cuit|q|quuit|kuit|qwit|qu\s?it|qutit|qwiut|\/quit|syut|quik|qutting|kuite|qauit|:q|:wq|wuit|qq|done|send|sent|sen|sends)$/i.test(message.trim());
